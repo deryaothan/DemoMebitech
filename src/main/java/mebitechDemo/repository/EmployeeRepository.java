63691477ea9e9e1bccb275f075f88ab33ea59368
deryaothan <deryaothan@hotmail.com>
2017-06-24 18:27:46 +0300

    update EmployeeRepository
    @Repository adding

--- src/main/java/mebitechDemo/repository/EmployeeRepository.java
+++ src/main/java/mebitechDemo/repository/EmployeeRepository.java
@@ -8,5 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @version 0.0.1
  * @since 24.06.2017
  */
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, String>{
 }
